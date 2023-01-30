bdbe7c4cc5675fd2c6f9f4735295d3686d42fc86
LaupWing <loc_strict@hotmail.com>
2023-01-30 10:51:54 +0100

    isntalled

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { SafeAreaView, Text, View } from "react-native"
 export default function App() {
    return (
       <SafeAreaView className="p-10 text-red-400">
-         <Text>Open up App.tsx to start working on your app!</Text>
+         <Text className="text-white">Open up App.tsx to start working on your app!</Text>
          <StatusBar style="auto" />
       </SafeAreaView>
    )
