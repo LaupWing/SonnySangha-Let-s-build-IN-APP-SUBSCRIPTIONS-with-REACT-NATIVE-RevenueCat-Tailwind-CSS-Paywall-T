bf7b05abb881dcfafbd06b7caa34b19213c9808f
LaupWing <loc_strict@hotmail.com>
2023-01-30 11:14:06 +0100

    upate

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,8 +1,10 @@
+import { StatusBar } from "expo-status-bar"
 import { View, Text, SafeAreaView } from "react-native"
 
 const HomeScreen = () => {
    return (
       <SafeAreaView className="">
+         {/* <StatusBar/> */}
          <Text>HomeScreen</Text>
       </SafeAreaView>
    )
