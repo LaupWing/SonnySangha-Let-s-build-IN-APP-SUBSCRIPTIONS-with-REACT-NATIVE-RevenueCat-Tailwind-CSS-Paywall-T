feb754f0b09caaf6646432710e162f5e03c8966c
LaupWing <loc_strict@hotmail.com>
2023-01-30 11:50:38 +0100

    udpate

--- screens/DemoScreen.tsx
+++ screens/DemoScreen.tsx
@@ -1,6 +1,16 @@
 import { View, Text, SafeAreaView } from "react-native"
+import { NativeStackNavigationProp } from "@react-navigation/native-stack"
+import { NativeStackNavigationProp } from "@react-navigation/native"
+import { RootStackParamsList } from "../App"
+
+export type NavigationProp = NativeStackNavigationProp<
+   RootStackParamsList,
+   "Demo"
+>
 
 const DemoScreen = () => {
+   const navigation = useN
+
    return (
       <SafeAreaView>
          <Text>DemoScreen</Text>
