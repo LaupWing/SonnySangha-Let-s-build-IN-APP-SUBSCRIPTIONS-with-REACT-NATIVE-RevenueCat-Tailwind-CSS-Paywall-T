280d16828bfac9cac3ec048ba280a9e77f5a43ca
LaupWing <loc_strict@hotmail.com>
2023-01-30 11:12:33 +0100

    udapte

---
+++ screens/HomeScreen.tsx
@@ -0,0 +1,10 @@
+import { View, Text, SafeAreaView } from "react-native"
+
+const HomeScreen = () => {
+   return (
+      <SafeAreaView className="">
+         <Text>HomeScreen</Text>
+      </SafeAreaView>
+   )
+}
+export default HomeScreen
