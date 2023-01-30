c46ff062e4c396ae78a17a68f5be9426292bd3a2
LaupWing <loc_strict@hotmail.com>
2023-01-30 10:48:09 +0100

    udpate

--- App.tsx
+++ App.tsx
@@ -1,11 +1,11 @@
 import { StatusBar } from "expo-status-bar"
-import { Text, View } from "react-native"
+import { SafeAreaView, Text, View } from "react-native"
 
 export default function App() {
    return (
-      <View>
+      <SafeAreaView className="p-10 text-red-400">
          <Text>Open up App.tsx to start working on your app!</Text>
          <StatusBar style="auto" />
-      </View>
+      </SafeAreaView>
    )
 }
