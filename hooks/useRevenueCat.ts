ec70a9d76f34f6a51b6f90f6047fc40a8349ed2e
LaupWing <loc_strict@hotmail.com>
2023-01-31 11:36:37 +0100

    update

---
+++ hooks/useRevenueCat.ts
@@ -0,0 +1,41 @@
+import { useEffect, useState } from "react"
+import { Platform } from "react-native"
+import { CustomerInfo, PurchasesOffering } from "react-native-purchases"
+import Purchases from "react-native-purchases/dist/purchases"
+
+const APIKeys ={
+   apple: "",
+   google: ""
+}
+
+const useRevenueCat = () => {
+   const [currentOffering, setCurrentOffering] = useState<PurchasesOffering|null>(null) 
+   const [customerInfo, setCustomerInfo] = useState<CustomerInfo|null>(null) 
+
+   const isProMember = customerInfo?.activeSubscriptions.includes("proMonthly") || customerInfo?.activeSubscriptions.includes("proYearly") 
+
+   useEffect(()=>{
+      const fetchData = async () =>{
+         Purchases.setDebugLogsEnabled(true)
+         if(Platform.OS === "android"){
+            await Purchases.configure({
+               apiKey: APIKeys.google
+            })
+         }else{
+            await Purchases.configure({
+               apiKey: APIKeys.apple
+            })
+         }
+
+         const offerings = await Purchases.getOfferings()
+         const customerInfo = await Purchases.getCustomerInfo()
+
+         setCurrentOffering(offerings.current)
+         setCustomerInfo(customerInfo)
+      }
+      fetchData().catch(console.error)
+   },[])
+
+   return null
+}
+export default useRevenueCat
